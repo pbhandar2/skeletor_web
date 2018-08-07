0045b3ba07098c817e105c24b7587fdbdfa67637
pbhandar2 <bhandaripranav94@gmail.com>
2018-08-07 19:41:32 -0400

    set up pm2 and launch a http and a https server now checking for https connection on ec2

---
+++ index.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var https = require('https');
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('hey');
+}).listen(process.env.PORT || 80, function() {
+  console.log('App listening on port %d', server.address().port);
+});
+
+var server = https.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('hey');
+}).listen(process.env.HTTPSPORT || 443, function() {
+  console.log('App listening on port %d', server.address().port);
+});
+
+

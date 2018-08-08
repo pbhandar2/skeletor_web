9cc7e15ab11c9af9cc2e4e0a1b42dfc91b04ebe9
pbhandar2 <bhandaripranav94@gmail.com>
2018-08-07 20:08:15 -0400

    testing https

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var http = require('http');
 var https = require('https');
 
-var server = http.createServer(function(req, res) {
+http.createServer(function(req, res) {
   res.writeHead(200);
   res.end('hey');
 }).listen(process.env.PORT || 80, function() {
   console.log('App listening on port %d', server.address().port);
 });
 
-var server = https.createServer(function(req, res) {
+https.createServer(function(req, res) {
   res.writeHead(200);
   res.end('hey');
 }).listen(process.env.HTTPSPORT || 443, function() {

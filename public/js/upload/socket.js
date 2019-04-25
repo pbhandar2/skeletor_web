6a64bdbef17dbef36db08b3a659bcfa8c0370e0b
pbhandar2 <bhandaripranav94@gmail.com>
2019-04-25 09:29:40 -0400

    fix socket bug

--- public/js/upload/socket.js
+++ public/js/upload/socket.js
@@ -12,6 +12,7 @@ socket.on(lambda_done_socket_name, function (file_name) {
 	//console.log("LMAMDA");
 	//console.log(file_name);
 	//console.log(queue);
+	uploadCompleted(file_name);
 	queue[file_name]["done"] = parseInt(queue[file_name]["done"]) + 1;
 	const width = (queue[file_name]["done"]/queue[file_name]["need"]) * 100;
 	//console.log(document.getElementById(`${file_name}-progress`));

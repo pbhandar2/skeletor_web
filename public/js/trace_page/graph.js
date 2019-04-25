648ed4d96a3708f740bfeef82df6ac97d96d4b19
pbhandar2 <bhandaripranav94@gmail.com>
2019-04-25 14:28:07 -0400

    remove comments from graph.js

--- public/js/trace_page/graph.js
+++ public/js/trace_page/graph.js
@@ -358,8 +358,8 @@ function zoomed() {
   var t = d3.event.transform;
   x.domain(t.rescaleX(x2).domain());
 
-  console.log(t.rescaleX(x2).domain());
-  console.log("this is x domain set");
+  //console.log(t.rescaleX(x2).domain());
+  //console.log("this is x domain set");
 
   let rescale = t.rescaleX(x2).domain()
   start_time = rescale[0]
@@ -420,8 +420,8 @@ function zoomed() {
   }
   focus.select(".axis--x").call(xAxis);
   context.select(".brush").call(brush.move, x.range().map(t.invertX, t));
-  console.log(x.range().map(t.invertX, t));
-  console.log("this is the brush.");
+  //console.log(x.range().map(t.invertX, t));
+  //console.log("this is the brush.");
 }
 
 function getTotalMetrics() {

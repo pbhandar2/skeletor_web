8739c1f8a79a9108ca5335f7483d5b2a980f3f24
pbhandar2 <bhandaripranav94@gmail.com>
2019-04-25 05:53:17 -0400

    debugging upload file

--- index.js
+++ index.js
@@ -173,6 +173,35 @@ app.post('/traces/:traceId', function(req, res){
 		}
 
     fs.rename(file.path, path.join(form.uploadDir, file.name), (error) => {
+      		if (error) console.log("An error occured while renaming and moving the file." + error);
+    			else {
+    				const file_object = {
+    					"name": file.name,
+    					"size": file.size,
+    					"timestamp": timestamp,
+    					"path": `/uploads/${req.params.traceId}/${file.name}_${timestamp}/${file.name}`
+    				};
+
+            // console.log("THIS IS THE FILE OBJECT.")
+          	// console.log(file_object)
+
+    				const aws = require("./library/aws");
+    		        const add_file_promise = aws.add_file(file_object, req.params.traceId, io);
+    		        add_file_promise.then((flag) => {
+    		            const traceProcessor = require("./library/traceProcessor");
+    		            const process_trace_file_promise = traceProcessor.processTraceFile(file_object, req.params.traceId, io);
+    			        process_trace_file_promise.then((flag) => {
+    			            let end = moment();
+    			            let diff = end.diff(start);
+    			            let f = moment.utc(diff).format("HH:mm:ss.SSS");
+    			            console.log(f);
+    			        }).catch((err) => {
+    			            console.log(err);
+    			        }).then(done, done);
+    		        }).catch((err) => {
+    		            console.log(err);
+    		        }).then(done, done);
+    			}
     });
 
   });
